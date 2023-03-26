c4af67e33b81bbbbdaccc28ca8e00765de109821
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-03-27 01:12:01 +0800

    feat: 使用 Texture.from 和 new Sprite 的方式新增一個精靈

--- index.js
+++ index.js
@@ -20,3 +20,12 @@ document.body.appendChild(app.view);
 // 新橧一個精靈到舞台中
 let cat = PIXI.Sprite.from('images/cat.png');
 app.stage.addChild(cat);
+
+// PIXI.Sprite.from 是 Pixi 提供簡易建立精靈的工廠方法，會自動建立了一個 texture 紋裡到 cache 中，檢查如下：
+console.log(PIXI.utils.TextureCache);
+
+// 使用 Texture.from 和 new Sprite 的方式新增一個精靈
+const texture = PIXI.Texture.from('images/cat.png');
+let cat2 = new PIXI.Sprite(texture);
+cat2.x = 100; // 改變精靈的 x 座標
+app.stage.addChild(cat2);
