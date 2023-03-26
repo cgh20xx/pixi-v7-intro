656da2f565ac5ea581940a45929d96c877635f3f
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-03-27 00:43:39 +0800

    feat: 新橧一個精靈到舞台中

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // 新增 Pixi 應用和舞台
 // PIXI.Application 文件：http://pixijs.download/release/docs/PIXI.Application.html
 // 注意：transparent 已棄用，改為 backgroundAlpha
-let app = new PIXI.Application({
+const app = new PIXI.Application({
   hello: true,
   width: 256, // default: 800
   height: 256, // default: 600
@@ -16,3 +16,7 @@ console.log(app);
 
 // app.view 會產生一個 canvas，你可以放到任何地方。
 document.body.appendChild(app.view);
+
+// 新橧一個精靈到舞台中
+let cat = PIXI.Sprite.from('images/cat.png');
+app.stage.addChild(cat);
