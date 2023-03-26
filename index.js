72b507e9861baef20ebe72fa8f19a20bbcea3ff2
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-03-27 01:35:41 +0800

    feat: 推薦！載入多張圖片的方式

--- index.js
+++ index.js
@@ -18,14 +18,30 @@ console.log(app);
 document.body.appendChild(app.view);
 
 // 新橧一個精靈到舞台中
-let cat = PIXI.Sprite.from('images/cat.png');
-app.stage.addChild(cat);
+// let cat = PIXI.Sprite.from('images/cat.png');
+// app.stage.addChild(cat);
 
 // PIXI.Sprite.from 是 Pixi 提供簡易建立精靈的工廠方法，會自動建立了一個 texture 紋裡到 cache 中，檢查如下：
-console.log(PIXI.utils.TextureCache);
+// console.log(PIXI.utils.TextureCache);
 
-// 使用 Texture.from 和 new Sprite 的方式新增一個精靈
-const texture = PIXI.Texture.from('images/cat.png');
-let cat2 = new PIXI.Sprite(texture);
-cat2.x = 100; // 改變精靈的 x 座標
-app.stage.addChild(cat2);
+// 使用 PIXI.Texture.from 和 new PIXI.Sprite 的方式新增一個精靈
+// const texture = PIXI.Texture.from('images/cat.png');
+// let cat2 = new PIXI.Sprite(texture);
+// cat2.x = 100; // 改變精靈的 x 座標
+// app.stage.addChild(cat2);
+
+// 使用 PIXI.Assets.add 新增預載的多張圖片 http://pixijs.download/release/docs/PIXI.Assets.html#add
+PIXI.Assets.add('cat', 'images/cat.png');
+PIXI.Assets.add('blob', 'images/blob.png');
+PIXI.Assets.add('door', 'images/door.png');
+
+// 使用 PIXI.Assets.load 載入指定圖片並返回 Promise http://pixijs.download/release/docs/PIXI.Assets.html#load
+const texturesPromise = PIXI.Assets.load(['cat', 'blob', 'door']);
+texturesPromise.then((texture) => {
+  console.log('圖片載入完成');
+  // console.log(texture);
+  // console.log(
+  //   'texture.cat === PIXI.utils.TextureCache.cat ?',
+  //   texture.cat === PIXI.utils.TextureCache.cat
+  // );
+});
